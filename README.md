496de672b77fd3faae496a11d5330ca9016d432c
TinyNiko <qwedd80@sina.com>
2021-01-27 23:52:03 +0800

    add dyn counter

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 LLVM TEST
 
+# Build
+
 cmake -G "Xcode"  -DLT_LLVM_INSTALL_DIR=$LLVM_DIR ../
+
+# static_reg.sh
+
+mov code to llvm project.
+
+## LLDB DEBUGE
+
+lldb -- $LLVM_DIR/bin/opt -S -load-pass-plugin <build_dir>/lib/libMBAAdd.dylib -passes=mba-add input_for_mba.ll -o out.ll
+
+## REFERENCE
+
+https://github.com/banach-space/llvm-tutor
--- run_test.sh
+++ run_test.sh
@@ -11,7 +11,16 @@ export LLVM_DIR=/Users/nikos/gr/llvm-project/build
 # clang output/instrumented.bc -o output/instrumentx
 
 # staticcallCounter
+# $LLVM_DIR/bin/clang -emit-llvm -c ./inputs/input_for_cc.c -o output/input_for_cc.bc
+#  # Xcode
+# $LLVM_DIR/bin/opt -load-pass-plugin ./build/lib/Debug/libStaticCall.dylib -passes="print<static-cc>" output/input_for_hello.bc -o output/instrumented.bc -debug
+# $LLVM_DIR/bin/llvm-dis output/instrumented.bc -o output/instrument.ll
+
+# dynamiccallCounter
 $LLVM_DIR/bin/clang -emit-llvm -c ./inputs/input_for_cc.c -o output/input_for_cc.bc
- # Xcode
-$LLVM_DIR/bin/opt -load-pass-plugin ./build/lib/Debug/libStaticCall.dylib -passes="print<static-cc>" output/input_for_hello.bc -o output/instrumented.bc -debug
+# Xcode
+$LLVM_DIR/bin/opt -load-pass-plugin ./build/lib/Debug/libDynCall.dylib -passes="dynamic-cc" output/input_for_cc.bc -o output/instrumented.bc -debug
 $LLVM_DIR/bin/llvm-dis output/instrumented.bc -o output/instrument.ll
+$LLVM_DIR/bin/lli output/instrumented.bc
+clang output/instrumented.bc -o output/instrumentx
+
